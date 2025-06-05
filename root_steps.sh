9e13e7d5fdc21c9e5f09ded8dc9ac6d69a5cf7f4
Ryan A. Colyer <ryancolyer@yahoo.com>
2025-06-05 13:45:25 -0400

    Add miniforge / runvenv / bin files

--- root_steps.sh
+++ root_steps.sh
@@ -6,9 +6,38 @@ cd "$SCRIPTDIR"
 
 fwupdmgr -y --no-reboot-check update
 
-apt install -y vim-gtk3 xserver-xorg-input-synaptics aptitude git cmake curl apt-file autoconf automake rustc cargo cheese clang clisp dos2unix doxygen eog libreoffice wine lftp gfortran ghc galculator gimp-data-extras golang-go gsfonts gv hexedit idle inotify-tools net-tools ispell kdenlive latex2rtf latexdraw llvm lyx w3m maxima wxmaxima mercurial mplayer vlc smplayer mpv mtools mypy neovim nmap nodejs oathtool obs-studio obs-plugins octave okular openssh-server openssh-sftp-server p7zip pandoc pdf2svg php ps2eps psutils pybind11-dev fftw-dev fftw2 libfftw3-bin libfftw3-dev python3-numpy python3-scipy python3-matplotlib python3-seaborn python3-setuptools python3-sympy python3-toml python3-sklearn qemu-system-x86 qtbase5-dev qtcreator rename radeontop retext gitk samba screen scribus spell sshfs subversion unrar webp whois wmctrl xsel
+apt install -y vim-gtk3 xserver-xorg-input-synaptics aptitude git cmake curl apt-file autoconf automake rustc cargo cheese clang clisp dos2unix doxygen eog libreoffice wine lftp gfortran ghc galculator gimp-data-extras golang-go gsfonts gv hexedit idle inotify-tools net-tools ispell kdenlive latex2rtf latexdraw llvm lyx w3m maxima wxmaxima mercurial mplayer vlc smplayer mpv mtools mypy neovim nmap nodejs oathtool obs-studio obs-plugins octave okular openssh-server openssh-sftp-server p7zip pandoc pdf2svg php ps2eps psutils pybind11-dev fftw-dev fftw2 libfftw3-bin libfftw3-dev python3-numpy python3-scipy python3-matplotlib python3-seaborn python3-setuptools python3-sympy python3-toml python3-sklearn qemu-system-x86 qtbase5-dev qtcreator rename radeontop retext gitk samba screen scribus spell sshfs subversion unrar webp whois wmctrl xsel rubber
 
 snap install chromium signal-desktop telegram-desktop typst
+snap refresh
+
+# Install runvenv
+cd /usr/local/src &&
+if [ -e runvenv ]
+then
+  cd runvenv &&
+  git pull
+else
+  git clone https://github.com/rcolyer/runvenv.git &&
+  cd runvenv
+fi &&
+make &&
+cp -d runvenv runipyth runpip /usr/bin/
+cd "$SCRIPTDIR"
+
+# Install miniforge3 if missing
+cd /usr/local/share &&
+if [ ! -e miniforge3 ]
+then
+  FILE="Miniforge3-$(uname)-$(uname -m).sh" &&
+  wget "https://github.com/conda-forge/miniforge/releases/latest/download/$FILE" &&
+  HOME="/usr/local/share" bash "$FILE" -b
+  rm -f "$FILE"
+else
+  # If miniforge needs updated on deployments later, put instructions here.
+  true # Do nothing for now
+fi
+cd "$SCRIPTDIR"
 
 # Slack
 
